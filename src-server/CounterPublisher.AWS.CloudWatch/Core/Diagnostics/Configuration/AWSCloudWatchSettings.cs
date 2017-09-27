fc5c8b691fbf4dbf9f666e8b3728b666efb7c690
JohnTube <brice2k5@gmail.com>
2017-09-27 17:19:34 +0100

    ADDED: files

---
+++ src-server/CounterPublisher.AWS.CloudWatch/Core/Diagnostics/Configuration/AWSCloudWatchSettings.cs
@@ -0,0 +1,162 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AWSCloudWatchSettings.cs" company="Exit Games GmbH">
+//   Copyright (c) Exit Games GmbH.  All rights reserved.
+// </copyright>
+// <summary>
+//   Amazon WebServices CloudWatch settings.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ExitGames.Diagnostics.Configuration
+{
+    #region using directives
+
+    using System;
+    using System.Configuration;
+
+    #endregion
+
+    // ReSharper disable once InconsistentNaming
+    public class AWSCloudWatchSettings : CounterSampleSenderSettings
+    {
+        #region Properties
+
+        /// <summary>
+        ///     The AWS Access Key associated with the account
+        /// </summary>
+        [ConfigurationProperty("awsCloudWatchAccessKey", IsRequired = true)]
+        public string AWSCloudWatchAccessKey
+        {
+            get { return (string)this["awsCloudWatchAccessKey"]; }
+            set { this["awsCloudWatchAccessKey"] = value; }
+        }
+
+        /// <summary>
+        ///     The AWS Secret Access Key associated with the account
+        /// </summary>
+        [ConfigurationProperty("awsCloudWatchSecretKey", IsRequired = true)]
+        public string AWSCloudWatchSecretKey
+        {
+            get { return (string)this["awsCloudWatchSecretKey"]; }
+            set { this["awsCloudWatchSecretKey"] = value; }
+        }
+
+        /// <summary>
+        ///     Regions and Endpoints: http://docs.aws.amazon.com/general/latest/gr/rande.html#cw_region
+        /// 
+        ///     Region Name	            Region	    Endpoint	                            Protocol
+        ///     US East (N. Virginia)	us-east-1	monitoring.us-east-1.amazonaws.com      HTTP and HTTPS
+        ///                                         logs.us-east-1.amazonaws.com            HTTPS
+        /// 
+        ///     US West (Oregon)	    us-west-2	monitoring.us-west-2.amazonaws.com      HTTP and HTTPS
+        ///                                         logs.us-west-2.amazonaws.com            HTTPS
+        /// 
+        ///     US West (N. California)	us-west-1	monitoring.us-west-1.amazonaws.com      HTTP and HTTPS
+        ///                                         logs.us-west-1.amazonaws.com            HTTPS
+        /// 
+        ///     EU (Ireland)	        eu-west-1	monitoring.eu-west-1.amazonaws.com      HTTP and HTTPS
+        ///                                         logs.eu-west-1.amazonaws.com            HTTPS
+        /// 
+        ///     EU (Frankfurt)	        eu-central-1    
+        ///                                         monitoring.eu-central-1.amazonaws.com   HTTP and HTTPS
+        ///                                         logs.eu-central-1.amazonaws.com         HTTPS
+        /// 
+        ///     Asia Pacific (Singapore)
+        ///                         	ap-southeast-1	
+        ///                                         monitoring.ap-southeast-1.amazonaws.com HTTP and HTTPS
+        ///                                         logs.ap-southeast-1.amazonaws.com       HTTPS
+        /// 
+        ///     Asia Pacific (Sydney)	ap-southeast-2	
+        ///                                         monitoring.ap-southeast-2.amazonaws.com HTTP and HTTPS
+        ///                                         logs.ap-southeast-2.amazonaws.com       HTTPS
+        /// 
+        ///     Asia Pacific (Tokyo)	ap-northeast-1	
+        ///                                         monitoring.ap-northeast-1.amazonaws.com HTTP and HTTPS
+        ///                                         logs.ap-northeast-1.amazonaws.com       HTTPS
+        ///     South America (Sao Paulo)	
+        ///                             sa-east-1	monitoring.sa-east-1.amazonaws.com	    HTTP and HTTPS
+        /// </summary>
+        [ConfigurationProperty("awsCloudWatchServiceUrl", IsRequired = true)]
+        public Uri AWSCloudWatchServiceUrl
+        {
+            get
+            {
+                return (Uri)this["awsCloudWatchServiceUrl"];
+            }
+            set
+            {
+                this["awsCloudWatchServiceUrl"] = value;
+                this["endpoint"] = value;
+            }
+        }
+
+        [ConfigurationProperty("endpoint", IsRequired = false)]
+        public new Uri Endpoint
+        {
+            get
+            {
+                return (Uri)this["endpoint"];
+            }
+            set
+            {
+                this["endpoint"] = value;
+                this["awsCloudWatchServiceUrl"] = value;
+            }
+        }
+        
+        /// <summary>
+        ///     Namespaces are containers for metrics. 
+        ///     All AWS services that provide Amazon CloudWatch data use a namespace string, 
+        ///     beginning with "AWS/". The following services push metric data points to CloudWatch.
+        /// 
+        ///     AWS Product	Namespace
+        ///     Auto Scaling: AWS/AutoScaling, AWS Billing: AWS/Billing, Amazon CloudFront: AWS/CloudFront,
+        ///     Amazon CloudSearch: AWS/CloudSearch, Amazon DynamoDB: AWS/DynamoDB, Amazon ElastiCache: AWS/ElastiCache,
+        ///     Amazon Elastic Block Store: AWS/EBS, Amazon Elastic Compute Cloud: AWS/EC2,
+        ///     Elastic Load Balancing: AWS/ELB, Amazon Elastic MapReduce: AWS/ElasticMapReduce,
+        ///     Amazon Kinesis: AWS/Kinesis, AWS OpsWorks: AWS/OpsWorks, Amazon Redshift: AWS/Redshift,
+        ///     Amazon Relational Database Service: AWS/RDS, Amazon Route 53: AWS/Route53,
+        ///     Amazon Simple Notification Service: AWS/SNS, Amazon Simple Queue Service: AWS/SQS,
+        ///     Amazon Simple Workflow Service: AWS/SWF, AWS Storage Gateway: AWS/StorageGateway
+        /// </summary>
+        [ConfigurationProperty("awsCloudWatchNamespace", IsRequired = true)]
+        public string AWSCloudWatchNamespace
+        {
+            get { return (string)this["awsCloudWatchNamespace"]; }
+            set { this["awsCloudWatchNamespace"] = value; }
+        }
+
+        /// <summary>
+        ///     If you want Photon metrics with format: Namespace.Metric -> to be written to
+        ///     Namespace = awsCloudWatchNamespace/Namespace, Name = Metric
+        /// </summary>
+        [ConfigurationProperty("autoNamespace", IsRequired = false, DefaultValue = false)]
+        public bool AutoNamespace
+        {
+            get { return (bool)this["autoNamespace"]; }
+            set { this["autoNamespace"] = value; }
+        }
+
+        /// <summary>
+        ///     Dimension of InstanceId
+        /// </summary>
+        [ConfigurationProperty("awsInstanceIdLookupUrl", IsRequired = false)]
+        public string AWSInstanceIdLookupUrl
+        {
+            get { return (string)this["awsInstanceIdLookupUrl"]; }
+            set { this["awsInstanceIdLookupUrl"] = value; }
+        }
+
+        /// <summary>
+        ///     Dimension of AutoScalingGroup
+        /// </summary>
+        [ConfigurationProperty("autoScalingConfigFilePath", IsRequired = false)]
+        public string AutoScalingConfigFilePath
+        {
+            get { return (string)this["autoScalingConfigFilePath"]; }
+            set { this["autoScalingConfigFilePath"] = value; }
+        }
+
+        #endregion
+    }
+}
---
+++ src-server/CounterPublisher.AWS.CloudWatch/Core/Diagnostics/Monitoring/Protocol/AWS/CloudWatch/AWSCloudWatchWriter.cs
@@ -0,0 +1,479 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AWSCloudWatchWriter.cs" company="Exit Games GmbH">
+//   Copyright (c) Exit Games GmbH.  All rights reserved.
+// </copyright>
+// <summary>
+//   Amazon WebServices CloudWatch writer.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ExitGames.Diagnostics.Monitoring.Protocol.AWS.CloudWatch
+{
+    #region using directives
+
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Globalization;
+    
+    using Amazon.CloudWatch;
+    using Amazon.CloudWatch.Model;
+    
+    using ExitGames.Logging;
+    using ExitGames.Diagnostics.Configuration;
+    
+    #endregion
+
+    /// <summary>
+    /// Publishes metric data points to Amazon CloudWatch. 
+    /// Amazon Cloudwatch associates the data points with the specified metric. 
+    /// If the specified metric does not exist, Amazon CloudWatch creates the metric. 
+    /// It can take up to fifteen minutes for a new metric to appear in calls to the list-metrics action.
+    /// 
+    /// The size of a put-metric-datarequest is limited to 8 KB for HTTP GET requests and 40 KB for HTTP POST requests.
+    /// 
+    /// Although you can publish data points with time stamps as granular as one-thousandth of a second, 
+    /// CloudWatch aggregates the data to a minimum granularity of one minute. 
+    /// CloudWatch records the average (sum of all items divided by number of items) of the values 
+    /// received for every 1-minute period, as well as number of samples, maximum value, 
+    /// and minimum value for the same time period.
+    /// </summary>
+    public class AWSCloudWatchWriter : ICounterSampleWriter
+    {
+        #region Constants and Fields
+
+        private static readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
+        public const int MaxMetricNameLentgh = 255;
+
+        public const int MinSendInterval = 60;
+
+        public const int MaxPayloadSize = 40*1024;
+
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1);
+
+        private static readonly Regex SpecialCharPattern = new Regex("[^a-zA-Z0-9]", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+        private static readonly StandardUnit[] DataMeasure =
+        {
+            StandardUnit.Bits, StandardUnit.Bytes, StandardUnit.Gigabits, StandardUnit.Gigabytes,
+            StandardUnit.Kilobits, StandardUnit.Kilobytes, StandardUnit.Megabits, StandardUnit.Megabytes,
+            StandardUnit.Terabits, StandardUnit.Terabytes
+        };
+        
+        private static readonly StandardUnit[] OtherMeasure =
+        {
+            StandardUnit.Microseconds, StandardUnit.Milliseconds, StandardUnit.Seconds,
+            StandardUnit.Count, StandardUnit.Percent
+        };
+
+        private readonly AWSCloudWatchSettings settings;
+        
+        private CounterSampleSenderBase sender;
+
+        private bool _disposed;
+
+        private string instanceId = Environment.MachineName;
+
+        private string autoScalingGroupName = null;
+
+        private IAmazonCloudWatch cloudWatchClient;
+        
+        #endregion
+
+        #region Properties
+
+        public bool Ready
+        {
+            get { return true; }
+        }
+
+        public string SenderId
+        {
+            get { return this.sender.SenderId; }
+        }
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public AWSCloudWatchWriter(AWSCloudWatchSettings s)
+        {
+            this.settings = s;
+        }
+
+        ~AWSCloudWatchWriter()
+        {
+            this.Dispose(false);
+        }
+
+        #endregion
+
+        #region Methods
+        
+        public void Start(CounterSampleSenderBase sender)
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException("Resource was disposed.");
+            }
+
+            if (this.sender != null)
+            {
+                throw new InvalidOperationException("Already started(), can't call for second time");
+            }
+
+            this.sender = sender;
+
+            if (this.sender.SendInterval < MinSendInterval)
+            {
+                throw new ArgumentOutOfRangeException("sender", "sender.SendInterval is out of range. Min value is " + MinSendInterval);
+            }
+
+            // Dimension of InstanceId
+            if (!String.IsNullOrEmpty(this.settings.AWSInstanceIdLookupUrl))
+            {
+                try
+                {
+                    this.instanceId = new WebClient().DownloadString(this.settings.AWSInstanceIdLookupUrl);
+                }
+                catch (Exception e)
+                {
+                    // This will fail if running machine is not in AWS EC2
+                    Log.Error(e);
+                    Log.WarnFormat(
+                        "Failed to retrieve AWS instance id. Use hostname {0} instead. Lookup URL was: {1}",
+                        this.instanceId,
+                        this.settings.AWSInstanceIdLookupUrl);
+                }
+            }
+
+            // Dimension of AutoScalingGroup
+            if (!String.IsNullOrEmpty(this.settings.AutoScalingConfigFilePath))
+            {
+                try
+                {
+                    var autoScalingConfigFile = new FileInfo(this.settings.AutoScalingConfigFilePath);
+                    if (!autoScalingConfigFile.Exists)
+                    {
+                        Log.WarnFormat("AutoScalingConfigFile not found: {0}",
+                            this.settings.AutoScalingConfigFilePath);
+                    }
+                    else
+                    {
+                        using (var sr = new StreamReader(autoScalingConfigFile.FullName))
+                        {
+                            this.autoScalingGroupName = sr.ReadToEnd().Trim();
+                        }
+                        
+                        Log.InfoFormat(
+                            "AutoScalingGroupName {0} read from config file {1}",
+                            this.autoScalingGroupName,
+                            autoScalingConfigFile.FullName);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Error(e);
+                    Log.WarnFormat(
+                        "Failed to read AutoScalingGroupName from config file {0}",
+                        this.settings.AutoScalingConfigFilePath);
+                }
+            }
+
+            // Amazon AWS CloudWatch Client init
+            this.cloudWatchClient =
+                new AmazonCloudWatchClient(
+                    this.settings.AWSCloudWatchAccessKey,
+                    this.settings.AWSCloudWatchSecretKey, new AmazonCloudWatchConfig
+                    {
+                        ServiceURL = this.settings.AWSCloudWatchServiceUrl.OriginalString
+                    });
+        }
+
+        public void Publish(CounterSampleCollection[] packages)
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException("Resource was disposed.");
+            }
+
+            if (!this.Ready)
+            {
+                sender.RaiseOnDisconnetedEvent();
+                return;
+            }
+
+            // cloud-watch only lets us send maximum of 20 pieces of metric data per request,
+            // so split in to groups of 20
+            var data = new List<MetricDatum>(packages.Length);
+            foreach (var package in packages)
+            {
+                var d = GetMetricFromPerformanceCounterReader(package);
+                if (d.MetricName.Length > MaxMetricNameLentgh)
+                {
+                    Log.WarnFormat("Metric is too long (max 255): {0}, {1}", d.MetricName, GetMetricDatumToString(d));
+                    continue;
+                }
+
+                data.Add(d);
+            }
+
+            PublishMetricData(data);
+        }
+
+        private void PublishMetricData(List<MetricDatum> data)
+        {
+            if (data.Count <= 0) return;
+
+            if (this.settings.AutoNamespace)
+            {
+                while (data.Count > 0)
+                {
+                    var first = data[data.Count - 1];
+                    data.RemoveAt(data.Count - 1);
+
+                    var nsi = first.MetricName.LastIndexOf('.');
+                    if (nsi > 0)
+                    {
+                        var ns = first.MetricName.Substring(0, nsi).Replace('.', '/');
+                        first.MetricName = first.MetricName.Substring(nsi + 1);
+
+                        var nons = new PutMetricDataRequest()
+                        {
+                            MetricData = new List<MetricDatum>()
+                            {
+                                first
+                            },
+                            Namespace = this.settings.AWSCloudWatchNamespace + '/' + ns
+                        };
+
+                        for (var i = data.Count - 1; i >= 0; i--)
+                        {
+                            var d = data[i];
+                            if (d.MetricName.StartsWith(ns))
+                            {
+                                d.MetricName = d.MetricName.Substring(nsi + 1);
+                                nons.MetricData.Add(d);
+                                data.RemoveAt(i);
+                            }
+                        }
+
+                        if (Log.IsDebugEnabled)
+                        {
+                            foreach (var d in nons.MetricData)
+                            {
+                                Log.DebugFormat("Report metric at ns://{0}: {1}, {2}", nons.Namespace, d.MetricName, GetMetricDatumToString(d));
+                            }
+                        }
+
+                        this.cloudWatchClient.PutMetricData(nons);
+                    }
+                    else
+                    {
+                        // Collect all without dots
+                        var nons = new PutMetricDataRequest()
+                        {
+                            MetricData = new List<MetricDatum>()
+                            {
+                                first
+                            },
+                            Namespace = this.settings.AWSCloudWatchNamespace
+                        };
+
+                        for (var i = data.Count - 1; i >= 0; i --)
+                        {
+                            if (data[i].MetricName.IndexOf('.') < 0)
+                            {
+                                nons.MetricData.Add(data[i]);
+                                data.RemoveAt(i);
+                            }
+                        }
+
+                        if (Log.IsDebugEnabled)
+                        {
+                            foreach (var d in nons.MetricData)
+                            {
+                                Log.DebugFormat("Report metric at ns://{0}, {1}, {2}", nons.Namespace, d.MetricName, GetMetricDatumToString(d));
+                            }
+                        }
+
+                        this.cloudWatchClient.PutMetricData(nons);
+                    }
+                }
+            }
+            else
+            {
+                if (Log.IsDebugEnabled)
+                {
+                    foreach (var d in data)
+                    {
+                        Log.DebugFormat("Report metric: {0}, {1}", d.MetricName, GetMetricDatumToString(d));
+                    }
+                }
+
+                this.cloudWatchClient.PutMetricData(
+                    new PutMetricDataRequest()
+                    {
+                        MetricData = data,
+                        Namespace = this.settings.AWSCloudWatchNamespace
+                    });
+            }
+        }
+
+        private static string GetMetricDatumToString(MetricDatum d)
+        {
+            var s = new StringBuilder();
+
+            s.Append("(Metric");
+
+            if (d.MetricName != null)
+            {
+                s.AppendFormat(", name = {0}", d.MetricName);
+            }
+
+            if (d.StatisticValues != null)
+            {
+                s.AppendFormat(", stats = (min {0}, max {1}, count {2}, sum {3})", 
+                    d.StatisticValues.Minimum,
+                    d.StatisticValues.Maximum,
+                    d.StatisticValues.SampleCount,
+                    d.StatisticValues.Sum);
+            }
+
+            if (d.StatisticValues == null)
+            {
+                s.AppendFormat(", val = {0}", d.Value);
+            }
+
+            if (d.Unit != null)
+            {
+                s.AppendFormat(", unit = {0}", d.Unit.Value);
+            }
+
+            s.AppendFormat(", ts = {0}", d.Timestamp.ToString(CultureInfo.InvariantCulture));
+            s.Append(')');
+
+            return s.ToString();
+        }
+
+        private MetricDatum GetMetricFromPerformanceCounterReader(CounterSampleCollection package)
+        {
+            var data = new MetricDatum()
+            {
+                MetricName = package.CounterName,
+                StatisticValues = new StatisticSet()
+                {
+                    Maximum = 0.0,
+                    Minimum = 0.0,
+                    SampleCount = 0.0,
+                    Sum = 0.0
+                },
+                Unit = StandardUnit.Count,
+                Timestamp = Epoch,
+                Dimensions = new List<Dimension>(3)
+            };
+
+            // Find unit
+            for (int i = 0; i < DataMeasure.Length; i++)
+            {
+                var u = DataMeasure[i];
+                if (data.MetricName.Contains(u.Value))
+                {
+                    data.Unit = u;
+                }
+            }
+
+            if (data.MetricName.IndexOf("PerSec", StringComparison.InvariantCultureIgnoreCase) > -1)
+            {
+                data.Unit = data.Unit.Value + "/Second";
+            }
+            else
+            {
+                for (int i = 0; i < OtherMeasure.Length; i++)
+                {
+                    var u = OtherMeasure[i];
+                    if (data.MetricName.Contains(u.Value))
+                    {
+                        data.Unit = u;
+                    }
+                }
+            }
+            
+            // Collect stats
+            foreach (var sample in package)
+            {
+                data.StatisticValues.Maximum = Math.Max(data.StatisticValues.Maximum, sample.Value);
+                data.StatisticValues.Minimum = Math.Min(data.StatisticValues.Minimum, sample.Value);
+                data.StatisticValues.SampleCount += 1.0;
+                data.StatisticValues.Sum += sample.Value;
+
+                if (data.Timestamp < sample.Timestamp)
+                {
+                    data.Timestamp = sample.Timestamp;
+                }
+            }
+
+            // Setup dimensions
+            if (!String.IsNullOrEmpty(this.instanceId))
+            {
+                data.Dimensions.Add(new Dimension()
+                {
+                    Name = "InstanceId",
+                    Value = this.instanceId
+                });
+            }
+
+            if (!String.IsNullOrEmpty(this.autoScalingGroupName))
+            {
+                data.Dimensions.Add(new Dimension()
+                {
+                    Name = "AutoScalingGroupName",
+                    Value = this.autoScalingGroupName
+                });
+            }
+
+            if (!String.IsNullOrEmpty(this.SenderId))
+            {
+                data.Dimensions.Add(new Dimension()
+                {
+                    Name = "SenderId",
+                    Value = this.SenderId
+                });
+            }
+
+            return data;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (this.cloudWatchClient != null)
+                    {
+                        this.cloudWatchClient.Dispose();
+                        this.cloudWatchClient = null;
+                    }
+
+                    this.sender = null;
+                }
+
+                _disposed = true;
+            }
+        }
+
+
+        #endregion
+    }
+}
